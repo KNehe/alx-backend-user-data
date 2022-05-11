732b5a3c552214d9a0a79d0883581f83af71e4b7
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-05-11 16:10:02 +0000

    encrypt password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module encrypt_password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Expects one string argument name password
+    and returns a salted, hashed password,
+    which is a byte string
+    """
+    return bcrypt.hashpw(password.encode('utf-8'),
+                         bcrypt.gensalt())
