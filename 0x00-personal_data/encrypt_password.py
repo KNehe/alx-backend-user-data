a8931c81fcb0f008145dd06be92460dacc33afef
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-05-11 16:13:15 +0000

    check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,3 +13,11 @@ def hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'),
                          bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Expects 2 arguments and returns a boolean.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'),
+                          hashed_password)
