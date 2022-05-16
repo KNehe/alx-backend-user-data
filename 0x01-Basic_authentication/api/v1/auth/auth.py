9982c801c7ea9b0d08c99198a33ed4e8bfa79f11
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-05-16 15:29:04 +0000

    require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,6 +23,10 @@ class Auth:
         if path[-1] != '/' and path + '/'\
                 in excluded_paths:
             return False
+        for p in excluded_paths:
+            if p.endswith('*') and\
+                    path.startswith(p[:-1]):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
