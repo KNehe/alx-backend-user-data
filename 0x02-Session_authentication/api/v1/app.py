2f4c95c7248da681d32cdff639926360325e98dd
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-05-18 09:44:57 +0000

    modify filter: exclude session login path else require it

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,15 +37,16 @@ def before_request() -> None:
     excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
             ]
-    if auth:
-        if auth.require_auth(request.path, excluded_paths):
-            if not auth.authorization_header(request):
-                abort(401)
-            if not auth.current_user(request):
-                abort(403)
-            request.current_user = auth.current_user(request)
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(request)\
+                and not auth.session_cookie(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
