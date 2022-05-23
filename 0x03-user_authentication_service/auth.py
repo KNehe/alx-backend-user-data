fe37ef554795e87a88444f5b87e179ee231e3ede
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-05-23 10:02:34 +0000

    create session id for a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,3 +59,19 @@ class Auth:
                     )
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Creates a session from a 
+        user and stores session id in
+        database
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+
+        return session_id
