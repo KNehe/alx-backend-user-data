93ceac3482c0d1645e9e9d72be38f1584fdab34c
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-05-23 11:05:28 +0000

    reset and update password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,5 +83,36 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    Generate a reset passowrd token for user
+    """
+    email = request.form.get('email')
+    session_id = AUTH.create_session(email)
+
+    if not session_id:
+        abort(403)
+
+    token = AUTH.get_reset_password_token(email)
+
+    return jsonfiy({"email": email, "reset_token": token})
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """Updates a user password"""
+    email = request.form.get('email')
+    token = request.form.get('reset_token')
+    password = request.form.get('new_password')
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except Exception:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
